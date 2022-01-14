8762916d426e9fa6d15cfb3ccf79a43c96e4ac8d
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-14 15:22:36 -0800

    removal of styled comp & cheesecake spinning

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Ingredients from './components/ingredients';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Ingredients from "./components/ingredients";
 
 export default function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Ingredients />
+      <img src={logo} className="App-logo" alt="logo" />
+      <Ingredients />
     </div>
   );
-};
+}
--- client/src/components/instructions.js
+++ client/src/components/instructions.js
@@ -1,15 +1,11 @@
 import React from "react";
 
-
 export default function Instructions() {
-    return (
-        <div className="instructions">
-        <h4>Instructions</h4>
-        <p>
-          Bake at 325°F for 50 mins with a steel pan or bit longer with glass
-        </p>
-        <p>How to know if ready? make sure the middle is not liquid</p>
-      </div>
+  return (
+    <div className="instructions">
+      <h4>Instructions</h4>
+      <p>Bake at 325°F for 50 mins with a steel pan or bit longer with glass</p>
+      <p>How to know if ready? make sure the middle is not liquid</p>
     </div>
   );
 }
