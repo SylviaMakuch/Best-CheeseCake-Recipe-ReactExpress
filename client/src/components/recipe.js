74ea6d04cce1082235335e3ab3e9cdca58197f7d
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-01-14 02:09:10 -0800

    app working!

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -1,25 +1,38 @@
 import React from "react";
-import './recipe.css';
+import "./recipe.css";
 import { useState, useEffect } from "react";
 
 export default function Recipe() {
-    const [cheesecake, setCheesecake] = useState([]);
-    
-    useEffect(() =>{
-        fetch("/api/ricottacheesecake")
-        .then(res => res.json())
-        .then(ingredients => setCheesecake(ingredients))
-        console.log(cheesecake)
-         }, []);
+  const [cheesecake, setCheesecake] = useState([]);
 
-    return (
-      <div className="recipe">
+  useEffect(() => {
+    fetch("/api/ricottacheesecake")
+      .then((res) => res.json())
+      .then((ingredients) => setCheesecake(ingredients));
+    console.log(cheesecake);
+  }, []);
+
+  return (
+    <div className="recipe">
+      
         <h3>Ingredients</h3>
         <ul>
-            {cheesecake.map(({amount, ingredient}, index) =>{
-                return <li key={index}>{amount}  {ingredient}</li>
-            })}
+          {cheesecake.map(({ amount, ingredient }, index) => {
+            return (
+              <li key={index}>
+                {amount} {ingredient}
+              </li>
+            );
+          })}
         </ul>
-      </div>
-    );
-  }
+    
+          <p>
+              Bake at 325°F for 50 mins with a steel pan or bit longer with glass
+          </p>
+          <p>
+              How to know if ready? make sure the middle is not liquid
+          </p>
+
+    </div>
+  );
+}
